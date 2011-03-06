b164313d48a4fbb3bbc3f2c58e408baf309b6a2a
bryanl <bryan@osesm.com>
2011-03-05 19:43:54 -0500

    the canvas now has a camera view.
    I'm using this to prove that we can do a surfaceview in
    a fragment. Next up with be a real GLview

--- gen/com/osesm/pifft/R.java
+++ gen/com/osesm/pifft/R.java
@@ -17,15 +17,19 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int canvas=0x7f060002;
-        public static final int gallery=0x7f060001;
-        public static final int text=0x7f060000;
+        public static final int canvas=0x7f070002;
+        public static final int gallery=0x7f070001;
+        public static final int switch_cam=0x7f070003;
+        public static final int text=0x7f070000;
     }
     public static final class layout {
         public static final int canvas_fragment=0x7f030000;
         public static final int gallery_fragment=0x7f030001;
         public static final int list_item=0x7f030002;
         public static final int main=0x7f030003;
+    }
+    public static final class menu {
+        public static final int camera_menu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
